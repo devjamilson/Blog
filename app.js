c7d2cab0bda2ac5f7c1c6c71dbc066efc86b6055
Jamilson Ferreira <83456401+devjamilson@users.noreply.github.com>
2022-10-14 20:27:30 -0300

    model categoria

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const bodyParser = require('body-parser')
 const admin = require('./routes/admin')
 //modulo padrão do node para manipular arquivos
 const path =  require('path')
-//const mongoose = require('mongoose')
+//config a conexão com o mongodb
+const mongoose = require('mongoose')
 const app = express()
 
 // Configurações
@@ -16,7 +17,12 @@ const app = express()
     app.engine('handlebars', handlebars.engine({defaultLayout: 'main'}))
     app.set('view engine', 'handlebars')
     //Mongoose
-       //em breve
+    mongoose.Promise = global.Promise
+    mongoose.connect('mongodb://localhost/blogapp').then(()=>{
+        console.log("Conectado ao Mongo")
+    }).catch((err)=>{
+        console.log("Erro ao se conectar" + err)
+    })
     //Public - arquivos estáticos
     //ou seja, estamos dizendo ao express que a pasta que guarda o nossos arquivos estático é a public
     app.use(express.static(path.join(__dirname, "public")))
@@ -26,7 +32,7 @@ const app = express()
    app.use('/admin', admin)
 
 // Outros
-const PORT = 8081
+const PORT = 3000
 
 app.listen(PORT, ()=>{
     console.log("Servidor Rodando")
---
+++ models/Categoria.js
@@ -0,0 +1,20 @@
+//estrutura do model categoria
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const Categoria = new Schema({
+    nome: {
+        type: String,
+        require: true
+    },
+    slug: {
+        type: String,
+        require: true
+    },
+    date:{
+        type: Date,
+        default: Date.now()
+    }
+})
+
+mongoose.model("categorias", Categoria)
